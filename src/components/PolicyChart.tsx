f232dbfa42fe5cafe6363a0e3e53c51c3d5d8af9
azariahtan <azariahtan.piano@gmail.com>
2026-09-09 23:37:00 +0800

    Update PolicyChart.tsx

--- src/components/PolicyChart.tsx
+++ src/components/PolicyChart.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"; 
 import { toast } from "sonner";
 import {
   ComposedChart,
